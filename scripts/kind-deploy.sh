e687519db9c872e5ea4b3c0fea2f894917c64070
MicaiahReid <micaiahreid@gmail.com>
2023-06-01 16:06:01 -0400

    set context on automated kubectl command

--- scripts/kind-deploy.sh
+++ scripts/kind-deploy.sh
@@ -2,5 +2,5 @@ kind create cluster --config=./templates/initial-config/kind.yaml && \
 docker pull hirosystems/stacks-blockchain-api:latest --platform=linux/amd64 && \
 kind load docker-image hirosystems/stacks-blockchain-api && \
 kind load docker-image stacks-network && \
-kubectl apply -f https://openebs.github.io/charts/openebs-operator.yaml && \
+kubectl --context kind-kind apply -f https://openebs.github.io/charts/openebs-operator.yaml && \
 kubectl apply -f ./templates/initial-config/storage-class.yaml
