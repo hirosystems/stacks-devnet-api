3866a64ecbf354e6b9704de6f6f12d142c4b832e
MicaiahReid <micaiahreid@gmail.com>
2023-06-02 08:51:17 -0400

    add context to kubectl commands

--- scripts/get-logs.sh
+++ scripts/get-logs.sh
@@ -1,5 +1,5 @@
-kubectl logs stacks-node --namespace $1 > ./logs/stacks-node.txt & \
-kubectl logs stacks-api --namespace $1 -c stacks-api-container > ./logs/stacks-api.txt & \
-kubectl logs stacks-api --namespace $1 -c stacks-api-postgres > ./logs/stacks-api-postgres.txt & \
-kubectl logs bitcoind-chain-coordinator --namespace $1 -c bitcoind-container > ./logs/bitcoin-node.txt & \
-kubectl logs bitcoind-chain-coordinator --namespace $1 -c chain-coordinator-container > ./logs/chain-coordinator.txt 
+kubectl --context kind-kind logs stacks-node --namespace $1 > ./logs/stacks-node.txt & \
+kubectl --context kind-kind logs stacks-api --namespace $1 -c stacks-api-container > ./logs/stacks-api.txt & \
+kubectl --context kind-kind logs stacks-api --namespace $1 -c stacks-api-postgres > ./logs/stacks-api-postgres.txt & \
+kubectl --context kind-kind logs bitcoind-chain-coordinator --namespace $1 -c bitcoind-container > ./logs/bitcoin-node.txt & \
+kubectl --context kind-kind logs bitcoind-chain-coordinator --namespace $1 -c chain-coordinator-container > ./logs/chain-coordinator.txt 
