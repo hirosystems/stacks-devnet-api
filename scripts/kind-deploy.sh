09a6369b52ff8cb39c1b9863fd9400ea20cc6d5a
MicaiahReid <micaiahreid@gmail.com>
2023-06-02 08:51:17 -0400

    add context to kubectl commands

--- scripts/kind-deploy.sh
+++ scripts/kind-deploy.sh
@@ -3,4 +3,4 @@ docker pull hirosystems/stacks-blockchain-api:latest --platform=linux/amd64 && \
 kind load docker-image hirosystems/stacks-blockchain-api && \
 kind load docker-image stacks-network && \
 kubectl --context kind-kind apply -f https://openebs.github.io/charts/openebs-operator.yaml && \
-kubectl apply -f ./templates/initial-config/storage-class.yaml
+kubectl --context kind-kind apply -f ./templates/initial-config/storage-class.yaml
