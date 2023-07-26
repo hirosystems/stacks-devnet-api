074a2ec60936f32b3d3cdcfdb84bb6e090da0b20
MicaiahReid <micaiahreid@gmail.com>
2023-07-26 10:32:17 -0400

    add handle_check_devnet endpoint

--- src/routes.rs
+++ src/routes.rs
@@ -105,6 +105,28 @@ pub async fn handle_get_devnet(
     }
 }
 
+pub async fn handle_check_devnet(
+    k8s_manager: StacksDevnetApiK8sManager,
+    network: &str,
+) -> Result<Response<Body>, Infallible> {
+    match k8s_manager.check_any_devnet_assets_exist(&network).await {
+        Ok(assets_exist) => match assets_exist {
+            true => Ok(Response::builder()
+                .status(StatusCode::OK)
+                .body(Body::empty())
+                .unwrap()),
+            false => Ok(Response::builder()
+                .status(StatusCode::from_u16(404).unwrap())
+                .body(Body::empty())
+                .unwrap()),
+        },
+        Err(e) => Ok(Response::builder()
+            .status(StatusCode::from_u16(e.code).unwrap())
+            .body(Body::try_from(e.message).unwrap())
+            .unwrap()),
+    }
+}
+
 pub async fn handle_try_proxy_service(
     remaining_path: &str,
     subroute: &str,
