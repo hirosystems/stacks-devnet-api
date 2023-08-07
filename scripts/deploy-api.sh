f5ff5e0a387ad8bd3ff04935e6510c60bbc81af0
MicaiahReid <micaiahreid@gmail.com>
2023-08-07 16:02:23 -0400

    fix: create namespace in deploy api script

--- scripts/deploy-api.sh
+++ scripts/deploy-api.sh
@@ -1,2 +1,3 @@
+kubectl --context kind-kind create namespace devnet
 kubectl --context kind-kind create configmap stacks-devnet-api-conf --from-file=./Config.toml --namespace devnet && \
 kubectl --context kind-kind apply -f ./templates/stacks-devnet-api.template.yaml
