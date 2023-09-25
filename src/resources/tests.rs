58c700bb9edeccbbac74ad0d837abb8a566a9e53
MicaiahReid <micaiahreid@gmail.com>
2023-09-25 09:32:07 -0400

    fix test descriptions

--- src/resources/tests.rs
+++ src/resources/tests.rs
@@ -6,7 +6,7 @@ use super::{
 use test_case::test_case;
 
 #[test_case(StacksDevnetConfigmap::BitcoindNode => is equal_to  "bitcoind".to_string(); "for BitcoinNode")]
-#[test_case(StacksDevnetConfigmap::StacksBlockchain => is equal_to  "stacks-blockchain".to_string(); "for StacksNode")]
+#[test_case(StacksDevnetConfigmap::StacksBlockchain => is equal_to  "stacks-blockchain".to_string(); "for StacksBlockchain")]
 #[test_case(StacksDevnetConfigmap::StacksBlockchainApi => is equal_to  "stacks-blockchain-api".to_string(); "for StacksBlockchainApi")]
 #[test_case(StacksDevnetConfigmap::StacksBlockchainApiPg => is equal_to  "stacks-blockchain-api-pg".to_string(); "for StacksBlockchainApiPg")]
 #[test_case(StacksDevnetConfigmap::DeploymentPlan => is equal_to  "deployment-plan".to_string(); "for DeploymentPlan")]
@@ -18,7 +18,7 @@ fn it_prints_correct_name_for_configmap(configmap: StacksDevnetConfigmap) -> Str
 }
 
 #[test_case(StacksDevnetPod::BitcoindNode => is equal_to  "bitcoind-chain-coordinator".to_string(); "for BitcoindNode")]
-#[test_case(StacksDevnetPod::StacksBlockchain => is equal_to  "stacks-blockchain".to_string(); "for StacksNode")]
+#[test_case(StacksDevnetPod::StacksBlockchain => is equal_to  "stacks-blockchain".to_string(); "for StacksBlockchain")]
 #[test_case(StacksDevnetPod::StacksBlockchainApi => is equal_to  "stacks-blockchain-api".to_string(); "for StacksBlockchainApi")]
 fn it_prints_correct_name_for_pod(pod: StacksDevnetPod) -> String {
     pod.to_string()
@@ -30,7 +30,7 @@ fn it_prints_correct_name_for_pvc(pvc: StacksDevnetPvc) -> String {
 }
 
 #[test_case(StacksDevnetService::BitcoindNode => is equal_to  "bitcoind-chain-coordinator".to_string(); "for BitcoindNode")]
-#[test_case(StacksDevnetService::StacksBlockchain => is equal_to  "stacks-blockchain".to_string(); "for StacksNode")]
+#[test_case(StacksDevnetService::StacksBlockchain => is equal_to  "stacks-blockchain".to_string(); "for StacksBlockchain")]
 #[test_case(StacksDevnetService::StacksBlockchainApi => is equal_to  "stacks-blockchain-api".to_string(); "for StacksBlockchainApi")]
 fn it_prints_correct_name_for_service(service: StacksDevnetService) -> String {
     service.to_string()
@@ -40,8 +40,8 @@ fn it_prints_correct_name_for_service(service: StacksDevnetService) -> String {
 #[test_case(StacksDevnetService::BitcoindNode, ServicePort::P2P => is equal_to  Some("18444".to_string()); "for BitcoindNode P2P port")]
 #[test_case(StacksDevnetService::BitcoindNode, ServicePort::Ingestion => is equal_to  Some("20445".to_string()); "for BitcoindNode Ingestion port")]
 #[test_case(StacksDevnetService::BitcoindNode, ServicePort::Control => is equal_to  Some("20446".to_string()); "for BitcoindNode Control port")]
-#[test_case(StacksDevnetService::StacksBlockchain, ServicePort::RPC => is equal_to  Some("20443".to_string()); "for StacksNode RPC port")]
-#[test_case(StacksDevnetService::StacksBlockchain, ServicePort::P2P => is equal_to  Some("20444".to_string()); "for StacksNode P2P port")]
+#[test_case(StacksDevnetService::StacksBlockchain, ServicePort::RPC => is equal_to  Some("20443".to_string()); "for StacksBlockchain RPC port")]
+#[test_case(StacksDevnetService::StacksBlockchain, ServicePort::P2P => is equal_to  Some("20444".to_string()); "for StacksBlockchain P2P port")]
 #[test_case(StacksDevnetService::StacksBlockchainApi, ServicePort::API => is equal_to  Some("3999".to_string()); "for StacksBlockchainApi API port")]
 #[test_case(StacksDevnetService::StacksBlockchainApi, ServicePort::Event => is equal_to  Some("3700".to_string()); "for StacksBlockchainApi Event port")]
 #[test_case(StacksDevnetService::StacksBlockchainApi, ServicePort::DB => is equal_to  Some("5432".to_string()); "for StacksBlockchainApi DB port")]
