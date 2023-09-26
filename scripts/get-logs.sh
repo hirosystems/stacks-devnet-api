7b52f93e82d46d1bb127dcc8f9c8c095f3426dc2
MicaiahReid <micaiahreid@gmail.com>
2023-09-26 12:27:50 -0400

    fix: api container spelling fix

--- scripts/get-logs.sh
+++ scripts/get-logs.sh
@@ -1,5 +1,5 @@
-kubectl --context kind-kind logs stacks-blockchain --namespace $1 > ./logs/stacks-blockchain.txt & \
-kubectl --context kind-kind logs stacks-blockchain-api --namespace $1 -c stacks-blockchain-api > ./logs/stack-blockchains-api.txt & \
-kubectl --context kind-kind logs stacks-blockchain-api --namespace $1 -c postgres > ./logs/stacks-blockchain-api-pg.txt & \
-kubectl --context kind-kind logs bitcoind-chain-coordinator --namespace $1 -c bitcoind > ./logs/bitcoin-node.txt & \
-kubectl --context kind-kind logs bitcoind-chain-coordinator --namespace $1 -c chain-coordinator > ./logs/chain-coordinator.txt 
+kubectl logs stacks-blockchain --namespace $1 > ./logs/stacks-blockchain.txt & \
+kubectl logs stacks-blockchain-api --namespace $1 -c stacks-blockchain-api > ./logs/stacks-blockchain-api.txt & \
+kubectl logs stacks-blockchain-api --namespace $1 -c postgres > ./logs/stacks-blockchain-api-pg.txt & \
+kubectl logs bitcoind-chain-coordinator --namespace $1 -c bitcoind > ./logs/bitcoin-node.txt & \
+kubectl logs bitcoind-chain-coordinator --namespace $1 -c chain-coordinator > ./logs/chain-coordinator.txt 
