dea090ea54899bca8a4e950bce73ab7280dbaa73
MicaiahReid <micaiahreid@gmail.com>
2023-09-28 20:14:30 -0400

    add new resources to main resource module

--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -1,18 +1,20 @@
 use self::{
-    configmap::StacksDevnetConfigmap, pod::StacksDevnetPod, pvc::StacksDevnetPvc,
-    service::StacksDevnetService,
+    configmap::StacksDevnetConfigmap, deployment::StacksDevnetDeployment, pod::StacksDevnetPod,
+    service::StacksDevnetService, stateful_set::StacksDevnetStatefulSet,
 };
 
 pub mod configmap;
+pub mod deployment;
 pub mod pod;
-pub mod pvc;
 pub mod service;
+pub mod stateful_set;
 
 pub enum StacksDevnetResource {
     Configmap(StacksDevnetConfigmap),
+    Deployment(StacksDevnetDeployment),
     Pod(StacksDevnetPod),
-    Pvc(StacksDevnetPvc),
     Service(StacksDevnetService),
+    StatefulSet(StacksDevnetStatefulSet),
     Namespace,
 }
 
