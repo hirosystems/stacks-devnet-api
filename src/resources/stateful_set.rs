b93d6930f3ee140cbd8aa70b41a4136152081850
Hugo CAILLARD <911307+hugocaillard@users.noreply.github.com>
2025-07-03 14:12:32 +0200

    feat: remove signer 1

--- src/resources/stateful_set.rs
+++ src/resources/stateful_set.rs
@@ -5,7 +5,6 @@ use strum::EnumIter;
 pub enum StacksDevnetStatefulSet {
     StacksBlockchainApi,
     StacksSigner0,
-    StacksSigner1,
 }
 
 impl fmt::Display for StacksDevnetStatefulSet {
@@ -13,7 +12,6 @@ impl fmt::Display for StacksDevnetStatefulSet {
         match self {
             StacksDevnetStatefulSet::StacksBlockchainApi => write!(f, "stacks-blockchain-api"),
             StacksDevnetStatefulSet::StacksSigner0 => write!(f, "stacks-signer-0"),
-            StacksDevnetStatefulSet::StacksSigner1 => write!(f, "stacks-signer-1"),
         }
     }
 }
@@ -21,14 +19,12 @@ impl fmt::Display for StacksDevnetStatefulSet {
 #[derive(EnumIter, Debug, Clone)]
 pub enum SignerIdx {
     Signer0,
-    Signer1,
 }
 
 impl fmt::Display for SignerIdx {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             SignerIdx::Signer0 => write!(f, "0"),
-            SignerIdx::Signer1 => write!(f, "1"),
         }
     }
 }
